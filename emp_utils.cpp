1d97d800208c523ed395961ee20a2959cb6803ca
easonycliu <easonliu@ucla.edu>
2025-04-22 14:33:41 -0700

    add two executables

---
+++ emp_utils.cpp
@@ -0,0 +1,359 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <array>
+#include <bitset>
+#include <chrono>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "emp-sh2pc/emp-sh2pc.h"
+
+using namespace emp;
+
+double get_cpu_time_ms() {
+	pid_t pid = getpid();
+	std::ifstream stat_file("/proc/" + std::to_string(pid) + "/stat");
+	if (!stat_file.is_open()) {
+		throw std::runtime_error("Failed to open /proc/[pid]/stat");
+	}
+
+	std::string token;
+	long utime_ticks = 0, stime_ticks = 0;
+	for (int i = 1; i <= 15; ++i) {
+		stat_file >> token;
+		if (i == 14)
+			utime_ticks = std::stol(token);
+		if (i == 15)
+			stime_ticks = std::stol(token);
+	}
+
+	long ticks_per_sec = sysconf(_SC_CLK_TCK);
+	double total_ms = (utime_ticks + stime_ticks) * 1000.0 / ticks_per_sec;
+	return total_ms;
+}
+
+template <std::size_t width, std::size_t bs>
+bool read_from_file(const std::string& file, std::vector<std::bitset<width>>& data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	constexpr std::size_t bytes = width / 8;
+
+	std::ifstream stream(file, std::ios::binary);
+	if (!stream.is_open()) {
+		std::cout << "Open file failed" << std::endl;
+		return false;
+	}
+
+	std::size_t read_size = 0;
+	std::array<std::byte, bytes * bs> buffer;
+
+	std::vector<std::byte> bdata;
+	while ((read_size = stream.readsome(reinterpret_cast<char*>(buffer.data()), sizeof(buffer))) > 0) {
+		bdata.insert(bdata.end(), buffer.begin(), buffer.begin() + read_size);
+	}
+
+	stream.close();
+
+	std::uintptr_t start = reinterpret_cast<std::uintptr_t>(bdata.data());
+	std::size_t size = bdata.size();
+
+	for (std::uintptr_t addr = start; addr < start + size; addr += bytes) {
+		std::byte* bitems = reinterpret_cast<std::byte*>(addr);
+		std::bitset<width> item(0);
+		for (std::size_t i = 0; i < bytes; i++) {
+			item |= std::bitset<width>(std::to_integer<int>(bitems[i])) << (i * 8);
+		}
+		data.push_back(item);
+	}
+
+	return true;
+}
+
+template <std::size_t width, std::size_t bs>
+bool write_to_file(const std::string& file, const std::vector<std::bitset<width>>& data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	constexpr std::size_t bytes = width / 8;
+
+	std::ofstream stream(file, std::ios::binary);
+	if (!stream.is_open()) {
+		std::cout << "Open file failed" << std::endl;
+		return false;
+	}
+
+	std::array<std::byte, bytes * bs> buffer;
+	stream.rdbuf()->pubsetbuf(reinterpret_cast<char*>(buffer.data()), sizeof(buffer));
+
+	for (std::bitset<width> item : data) {
+		for (std::size_t i = 0; i < bytes; i++) {
+			std::byte bitem = static_cast<std::byte>((item >> (i * 8)).to_ulong());
+			stream.write(reinterpret_cast<const char*>(&bitem), sizeof(bitem));
+		}
+	}
+
+	stream.flush();
+	stream.close();
+
+	return true;
+}
+
+std::size_t get_other_input_size(int party, char* problem_name, std::size_t problem_size) {
+	if (strcmp(problem_name, "merge_sorted") == 0) {
+		return problem_size * 4;
+	} else if (strcmp(problem_name, "full_sort") == 0) {
+		return problem_size * 4;
+	} else if (strcmp(problem_name, "loop_join") == 0) {
+		return problem_size * 4;
+	} else if (strcmp(problem_name, "matrix_vector_multiply") == 0) {
+		if (party == ALICE) {
+			return problem_size;
+		} else {
+			return problem_size * problem_size;
+		}
+	} else {
+		std::cerr << "Unknown problem name " << problem_name << std::endl;
+		std::abort();
+	}
+}
+
+template <std::size_t width>
+void encrypt_file(int party, std::size_t other_input_size, NetIO& io, const std::vector<std::bitset<width>>& input_data,
+				  std::vector<Integer>& output_data) {
+	std::vector<Integer> alice_output_data;
+	std::vector<Integer> bob_output_data;
+
+	std::size_t iters = std::max(input_data.size(), other_input_size);
+	for (std::size_t i = 0; i < iters; i++) {
+		alice_output_data.push_back(
+			Integer((party == ALICE && i < input_data.size()) ? input_data[i] : std::bitset<width>(0), ALICE));
+		bob_output_data.push_back(
+			Integer((party == BOB && i < input_data.size()) ? input_data[i] : std::bitset<width>(0), BOB));
+	}
+	io.flush();
+	output_data.insert(output_data.end(), alice_output_data.begin(),
+					   alice_output_data.begin() + ((party == ALICE) ? input_data.size() : other_input_size));
+	output_data.insert(output_data.end(), bob_output_data.begin(),
+					   bob_output_data.begin() + ((party == BOB) ? input_data.size() : other_input_size));
+}
+
+template <std::size_t width>
+void decrypt_file(int party, const std::vector<Integer>& input_data, std::vector<std::bitset<width>>& output_data) {
+	constexpr std::size_t bs = 4096;
+	for (std::size_t i = 0; i < input_data.size(); i += bs) {
+		Integer batch(std::vector<Bit>(0));
+		for (std::size_t j = 0; j < bs; j++) {
+			if (i + j < input_data.size()) {
+				batch.bits.insert(batch.bits.end(), input_data[i + j].bits.begin(), input_data[i + j].bits.end());
+			}
+		}
+
+		std::bitset<width* bs> bbatch = batch.reveal<width * bs>();
+		std::size_t output_size = std::min(bs, input_data.size() - i);
+		for (std::size_t j = 0; j < width * output_size; j += width) {
+			std::bitset<width> item(0);
+			for (std::size_t k = 0; k < width; k++) {
+				item.set(k, bbatch[j + k]);
+			}
+			output_data.push_back(item);
+		}
+	}
+}
+
+template <std::size_t width>
+void merge_sorted(int party, std::size_t problem_size, const std::vector<Integer>& input_data,
+				  std::vector<Integer>& output_data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	constexpr std::size_t bytes = width / 8;
+
+	std::vector<Integer> key;
+	std::vector<Integer> value;
+
+	for (std::size_t i = 0; i < input_data.size(); i += 4) {
+		key.push_back(input_data[i]);
+
+		Integer vitem(std::vector<Bit>(input_data[i + 1].bits.begin(), input_data[i + 1].bits.end()));
+		vitem.bits.insert(vitem.bits.end(), input_data[i + 2].bits.begin(), input_data[i + 2].bits.end());
+		vitem.bits.insert(vitem.bits.end(), input_data[i + 3].bits.begin(), input_data[i + 3].bits.end());
+		value.push_back(vitem);
+	}
+
+	bitonic_merge(key.data(), value.data(), 0, key.size(), true);
+
+	for (std::size_t i = 0; i != key.size(); i++) {
+		output_data.push_back(key[i]);
+		output_data.push_back(Integer(std::vector<Bit>(value[i].bits.begin(), value[i].bits.begin() + width)));
+		output_data.push_back(
+			Integer(std::vector<Bit>(value[i].bits.begin() + width, value[i].bits.begin() + 2 * width)));
+		output_data.push_back(
+			Integer(std::vector<Bit>(value[i].bits.begin() + 2 * width, value[i].bits.begin() + 3 * width)));
+	}
+}
+
+template <std::size_t width>
+void full_sort(int party, std::size_t problem_size, const std::vector<Integer>& input_data,
+			   std::vector<Integer>& output_data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	constexpr std::size_t bytes = width / 8;
+
+	std::vector<Integer> key;
+	std::vector<Integer> value;
+
+	for (std::size_t i = 0; i < input_data.size(); i += 4) {
+		key.push_back(input_data[i]);
+
+		Integer vitem(std::vector<Bit>(input_data[i + 1].bits.begin(), input_data[i + 1].bits.end()));
+		vitem.bits.insert(vitem.bits.end(), input_data[i + 2].bits.begin(), input_data[i + 2].bits.end());
+		vitem.bits.insert(vitem.bits.end(), input_data[i + 3].bits.begin(), input_data[i + 3].bits.end());
+		value.push_back(vitem);
+	}
+
+	bitonic_sort(key.data(), value.data(), 0, key.size(), false);
+
+	for (std::size_t i = 0; i != key.size(); i++) {
+		output_data.push_back(key[i]);
+		output_data.push_back(Integer(std::vector<Bit>(value[i].bits.begin(), value[i].bits.begin() + width)));
+		output_data.push_back(
+			Integer(std::vector<Bit>(value[i].bits.begin() + width, value[i].bits.begin() + 2 * width)));
+		output_data.push_back(
+			Integer(std::vector<Bit>(value[i].bits.begin() + 2 * width, value[i].bits.begin() + 3 * width)));
+	}
+}
+
+template <std::size_t width>
+void loop_join(int party, std::size_t problem_size, const std::vector<Integer>& input_data,
+			   std::vector<Integer>& output_data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	if (input_data.size() % 8 != 0) {
+		std::cerr << "Input data size must be multiple of 8" << std::endl;
+		return;
+	}
+
+	constexpr std::size_t bytes = width / 8;
+
+	Integer zero(width, 0, PUBLIC);
+
+	std::vector<Integer>::const_iterator table1_input_data_begin = input_data.begin();
+	std::vector<Integer>::const_iterator table1_input_data_end = input_data.begin() + input_data.size() / 2;
+	std::vector<Integer>::const_iterator table2_input_data_begin = input_data.begin() + input_data.size() / 2;
+	std::vector<Integer>::const_iterator table2_input_data_end = input_data.end();
+
+	for (std::vector<Integer>::const_iterator i = table1_input_data_begin; i != table1_input_data_end; i += 4) {
+		for (std::vector<Integer>::const_iterator j = table2_input_data_begin; j != table2_input_data_end; j += 4) {
+			Bit valid = i->geq(*j);
+			Integer valid_int(std::vector<Bit>(1, !valid));
+			valid_int.resize(width, false);
+			output_data.push_back(valid_int);
+			output_data.push_back(i->select(valid, zero));
+			output_data.push_back((i + 1)->select(valid, zero));
+			output_data.push_back((i + 2)->select(valid, zero));
+			output_data.push_back((i + 3)->select(valid, zero));
+			output_data.push_back(j->select(valid, zero));
+			output_data.push_back((j + 1)->select(valid, zero));
+			output_data.push_back((j + 2)->select(valid, zero));
+			output_data.push_back((j + 3)->select(valid, zero));
+		}
+	}
+}
+
+template <std::size_t width>
+void matrix_vector_multiply(int party, std::size_t problem_size, const std::vector<Integer>& input_data,
+							std::vector<Integer>& output_data) {
+	static_assert(width % 8 == 0, "Width must be multiple of 8");
+
+	constexpr std::size_t bytes = width / 8;
+
+	std::vector<Integer> vector;
+	std::vector<Integer> matrix;
+
+	for (std::size_t i = 0; i < input_data.size(); i++) {
+		if (i < problem_size * problem_size) {
+			matrix.push_back(input_data[i]);
+		} else {
+			vector.push_back(input_data[i]);
+		}
+	}
+
+	for (std::size_t i = 0; i < problem_size; i++) {
+		Integer result(width, 0);
+		for (std::size_t j = 0; j < problem_size; j++) {
+			result = result + (matrix[i * problem_size + j] * vector[j]);
+		}
+		output_data.push_back(result);
+	}
+}
+
+int main(int argc, char** argv) {
+	if (argc != 8) {
+		std::cout << "Usage: " << argv[0]
+				  << " [problem_name] [problem_size] [party] [port] [other_ip] [input_file] [output_file]" << std::endl;
+		return 1;
+	}
+
+	char* problem_name = argv[1];
+	std::size_t problem_size = std::stoull(argv[2]);
+	int party = atoi(argv[3]);
+	int port = atoi(argv[4]);
+	char* other_ip = argv[5];
+	char* input_file = argv[6];
+	char* output_file = argv[7];
+
+	constexpr std::size_t width = 32;
+	constexpr std::size_t bs = 4096;
+
+	std::vector<std::bitset<width>> input_data;
+	read_from_file<width, bs>(input_file, input_data);
+
+	NetIO io(party == ALICE ? nullptr : other_ip, port, true);
+	setup_semi_honest(&io, party);
+
+	double start_cpu_time = get_cpu_time_ms();
+
+	std::chrono::high_resolution_clock::time_point encrypt_start = std::chrono::high_resolution_clock::now();
+	std::vector<Integer> input_data_encrypt;
+	encrypt_file(party, get_other_input_size(party, problem_name, problem_size), io, input_data, input_data_encrypt);
+	std::chrono::high_resolution_clock::time_point encrypt_end = std::chrono::high_resolution_clock::now();
+	std::cout << "Encrypt time: "
+			  << std::chrono::duration_cast<std::chrono::milliseconds>(encrypt_end - encrypt_start).count() << " ms"
+			  << std::endl;
+
+	std::vector<Integer> output_data_encrypt;
+	if (strcmp(problem_name, "merge_sorted") == 0) {
+		merge_sorted<width>(party, problem_size, input_data_encrypt, output_data_encrypt);
+	} else if (strcmp(problem_name, "full_sort") == 0) {
+		full_sort<width>(party, problem_size, input_data_encrypt, output_data_encrypt);
+	} else if (strcmp(problem_name, "loop_join") == 0) {
+		loop_join<width>(party, problem_size, input_data_encrypt, output_data_encrypt);
+	} else if (strcmp(problem_name, "matrix_vector_multiply") == 0) {
+		matrix_vector_multiply<width>(party, problem_size, input_data_encrypt, output_data_encrypt);
+	} else {
+		std::cerr << "Unknown problem name" << std::endl;
+		return 1;
+	}
+
+	std::chrono::high_resolution_clock::time_point calc_end = std::chrono::high_resolution_clock::now();
+	std::cout << "Calc time: " << std::chrono::duration_cast<std::chrono::milliseconds>(calc_end - encrypt_end).count()
+			  << " ms" << std::endl;
+
+	std::vector<std::bitset<width>> output_data;
+	decrypt_file(party, output_data_encrypt, output_data);
+	std::chrono::high_resolution_clock::time_point decrypt_end = std::chrono::high_resolution_clock::now();
+	std::cout << "Decrypt time: "
+			  << std::chrono::duration_cast<std::chrono::milliseconds>(decrypt_end - calc_end).count() << " ms"
+			  << std::endl;
+
+	std::cout << "Total time: "
+			  << std::chrono::duration_cast<std::chrono::milliseconds>(decrypt_end - encrypt_start).count() << " ms"
+			  << std::endl;
+
+	double end_cpu_time = get_cpu_time_ms();
+	std::cout << "Total cpu time: " << end_cpu_time - start_cpu_time << " ms" << std::endl;
+
+	write_to_file<width, bs>(output_file, output_data);
+}
---
+++ example_input.cpp
@@ -0,0 +1,183 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <string>
+#include <utility>
+#include <vector>
+
+template <typename T, std::size_t width>
+void write(std::ofstream& stream, const T value) {
+	static_assert(sizeof(T) >= width, "Size of T must be greater than or equal to width");
+	stream.write(reinterpret_cast<const char*>(&value), width);
+}
+
+void write_record(std::ofstream& stream, const std::uint64_t value) {
+	write<std::uint64_t, 4>(stream, value);
+	write<std::uint64_t, 4>(stream, 0);
+	write<std::uint64_t, 4>(stream, 0);
+	write<std::uint64_t, 4>(stream, 0);
+}
+
+int main(int argc, char** argv) {
+	if (argc != 3 && argc != 4) {
+		std::cout << "Usage: " << argv[0] << " problem_name problem_size [option]" << std::endl;
+		return 0;
+	}
+	std::string problem_name(argv[1]);
+	int input_size = std::stoi(std::string(argv[2]));
+
+	std::string option;
+	if (argc == 5) {
+		option = argv[4];
+	}
+
+	std::ofstream garbler_file(problem_name + "_" + std::to_string(input_size) + "_garbler.input");
+	std::ofstream evaluator_file(problem_name + "_" + std::to_string(input_size) + "_evaluator.input");
+	std::ofstream expected_file(problem_name + "_" + std::to_string(input_size) + ".expected");
+
+	if (problem_name == "merge_sorted") {
+		for (std::uint64_t i = 0; i != input_size * 2; i++) {
+			if (i < input_size) {
+				write_record(garbler_file, 2 * i);
+			} else {
+				write_record(evaluator_file, 2 * (2 * input_size - i - 1) + 1);
+			}
+			write_record(expected_file, i);
+		}
+	} else if (problem_name == "full_sort") {
+		if (option == "") {
+			for (std::uint64_t i = 0; i != input_size * 2; i++) {
+				if (i < input_size) {
+					write_record(garbler_file, 2 * i);
+				} else {
+					write_record(evaluator_file, 2 * (2 * input_size - i - 1) + 1);
+				}
+				write_record(expected_file, i);
+			}
+		} else if (option == "random") {
+			std::vector<std::uint32_t> sorted(2 * input_size);
+			for (std::uint64_t i = 0; i != sorted.size(); i++) {
+				sorted[i] = static_cast<std::uint32_t>(i);
+			}
+			std::vector<std::uint32_t> array(sorted);
+			std::random_shuffle(array.begin(), array.end());
+			for (std::uint64_t i = 0; i != input_size * 2; i++) {
+				if (i < input_size) {
+					write_record(garbler_file, array[i]);
+				} else {
+					write_record(evaluator_file, array[i]);
+				}
+				write_record(expected_file, sorted[i]);
+			}
+		} else {
+			std::cerr << "Unknown option " << option << std::endl;
+		}
+	} else if (problem_name == "loop_join") {
+		std::vector<std::uint32_t> table1_keys(input_size);
+		std::iota(table1_keys.begin(), table1_keys.end(), 0);
+		std::vector<std::uint32_t> table2_keys(input_size);
+		std::iota(table2_keys.begin(), table2_keys.end(), 0);
+
+		for (std::uint64_t i = 0; i != table1_keys.size(); i++) {
+			write_record(garbler_file, table1_keys[i]);
+		}
+
+		for (std::uint64_t i = 0; i != table2_keys.size(); i++) {
+			write_record(evaluator_file, table2_keys[i]);
+		}
+
+		std::size_t join_size = table1_keys.size() * table2_keys.size();
+		for (std::uint64_t i = 0, k = 0; i != table1_keys.size(); i++) {
+			for (std::uint64_t j = 0; j != table2_keys.size(); j++, k++) {
+				bool valid = (table1_keys[i] < table2_keys[j]);
+				if (valid) {
+					write<std::uint32_t, 4>(expected_file, 1);
+					write_record(expected_file, table1_keys[i]);
+					write_record(expected_file, table2_keys[j]);
+				} else {
+					write<std::uint32_t, 4>(expected_file, 0);
+					write_record(expected_file, 0);
+					write_record(expected_file, 0);
+				}
+			}
+		}
+	} else if (problem_name == "matrix_multiply") {
+		if (option == "") {
+			for (std::uint64_t i = 0; i != input_size; i++) {
+				for (std::uint64_t j = 0; j != input_size; j++) {
+					std::uint8_t elem = (i == j) ? 1 : 0;
+					write<std::uint8_t, 1>(garbler_file, elem);
+					write<std::uint8_t, 1>(evaluator_file, elem);
+					write<std::uint16_t, 2>(expected_file, elem);
+				}
+			}
+		} else if (option == "random") {
+			std::default_random_engine generator;
+			std::uniform_int_distribution<std::uint8_t> distribution(0, UINT8_MAX);
+			std::vector<std::uint8_t> a(input_size * input_size);
+			for (std::size_t i = 0; i != a.size(); i++) {
+				a[i] = distribution(generator);
+				write<std::uint8_t, 1>(garbler_file, a[i]);
+			}
+			std::vector<std::uint8_t> b(input_size * input_size);
+			for (std::size_t i = 0; i != b.size(); i++) {
+				b[i] = distribution(generator);
+				write<std::uint8_t, 1>(evaluator_file, b[i]);
+			}
+			for (std::size_t i = 0; i != input_size; i++) {
+				for (std::size_t j = 0; j != input_size; j++) {
+					std::uint16_t elem = 0;
+					for (std::size_t k = 0; k != input_size; k++) {
+						elem += static_cast<std::uint16_t>(a[i * input_size + k]) *
+								static_cast<std::uint16_t>(b[j * input_size + k]);
+					}
+					write<std::uint16_t, 2>(expected_file, elem);
+				}
+			}
+		} else {
+			std::cerr << "Unknown option " << option << std::endl;
+		}
+	} else if (problem_name == "matrix_vector_multiply") {
+		if (option == "") {
+			for (std::uint64_t i = 0; i != input_size; i++) {
+				std::uint8_t elem = static_cast<std::uint8_t>(i);
+				write<std::uint32_t, 4>(evaluator_file, elem);
+				write<std::uint32_t, 4>(expected_file, elem);
+			}
+			for (std::uint64_t i = 0; i != input_size; i++) {
+				for (std::uint64_t j = 0; j != input_size; j++) {
+					std::uint8_t elem = (i == j) ? 1 : 0;
+					write<std::uint32_t, 4>(garbler_file, elem);
+				}
+			}
+		} else if (option == "random") {
+			std::default_random_engine generator;
+			std::uniform_int_distribution<std::uint8_t> distribution(0, UINT8_MAX);
+			std::vector<std::uint8_t> vector(input_size);
+			for (std::size_t i = 0; i != input_size; i++) {
+				vector[i] = distribution(generator);
+				write<std::uint32_t, 4>(evaluator_file, vector[i]);
+			}
+			for (std::size_t i = 0; i != input_size; i++) {
+				std::uint16_t expected_elem = 0;
+				for (std::size_t j = 0; j != input_size; j++) {
+					std::uint8_t matrix_elem = distribution(generator);
+					write<std::uint32_t, 4>(garbler_file, matrix_elem);
+					expected_elem += static_cast<std::uint16_t>(matrix_elem) * static_cast<std::uint16_t>(vector[j]);
+				}
+				write<std::uint32_t, 4>(expected_file, expected_elem);
+			}
+		} else {
+			std::cerr << "Unknown option " << option << std::endl;
+		}
+	} else {
+		std::cerr << "Unknown problem " << problem_name << std::endl;
+	}
+
+	return 0;
+}
