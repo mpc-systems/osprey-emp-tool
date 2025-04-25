5bb287eb4743141992da86b7b7054352f5e31eab
easonycliu <easonliu@ucla.edu>
2025-04-25 13:03:27 -0700

    increase buffer size

--- emp-tool/utils/constants.h
+++ emp-tool/utils/constants.h
@@ -4,7 +4,7 @@ namespace emp {
 	const static int AES_BATCH_SIZE = 8;
 	const static int HASH_BUFFER_SIZE = 1024 * 8;
 	const static int NETWORK_BUFFER_SIZE2 = 1024 * 32;
-	const static int NETWORK_BUFFER_SIZE = 1024 * 1024;
+	const static int NETWORK_BUFFER_SIZE = 1024 * 1024 * 4;
 	const static int FILE_BUFFER_SIZE = 1024 * 16;
 	const static int CHECK_BUFFER_SIZE = 1024 * 8;
 
