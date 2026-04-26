ae1344a05d01b5358f77b7e960f6df6dd0f3fd5b
easonycliu <easonliu@ucla.edu>
2026-04-26 09:22:25 -0700

    update senate pwd

--- senate_tpc_h/pwd.cpp
+++ senate_tpc_h/pwd.cpp
@@ -4,6 +4,10 @@ namespace senate_pwd {
 		return problem_size * 9;
 	}
 
+	void instrument_print() {
+		printf("Senate PWD Join and Aggregate Operator\n");
+	}
+
 	template <std::size_t width>
 	void join_and_aggregate(
 		int party, std::size_t problem_size, const std::vector<Integer<width>>& input_data, std::vector<Integer<width>>& output_data
@@ -23,11 +27,17 @@ namespace senate_pwd {
 
 		bitonic_merge(key.data(), value.data(), 0, key.size(), true);
 
+		output_data.resize(key.size());
+
 		// Everything is sorted now, do a PSU.
+		instrument_print();
 		for (int i = 0; i < key.size() - 1; ++i) {
 			Bit equals = key[i].equal(key[i+1]);
 			output_data[i] = key[i].select(equals, Integer<width>(0, PUBLIC));
+			printf("Finished key %d\n", i);
 		}
+		output_data[key.size() - 1] = key[key.size() - 1];
+		printf("Finished last key\n");
 	}
 
 }
